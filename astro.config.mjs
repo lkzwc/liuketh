11c8f4db8d7cf690488738402046b24b2bacd453
流客 <lkzwc@outlook.com>
2024-01-29 10:33:01 +0000

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
+
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [
-        tailwind({
-          // 示例：在每个页面上禁用注入基本的`base.css`导入。
-          // 如果你需要定义或导入你自己的 "base.css"，这很有用。
-          applyBaseStyles: false,
-        }),
-      ],
+  integrations: [tailwind({
+    // 示例：在每个页面上禁用注入基本的`base.css`导入。
+    // 如果你需要定义或导入你自己的 "base.css"，这很有用。
+    applyBaseStyles: false
+  }), mdx()]
 });
