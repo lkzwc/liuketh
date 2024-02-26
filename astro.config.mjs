0eaa2cd55bf2bb82c58e3d67757b33735c7c33b7
lkzwc <lkzwc@outlook.com>
2024-02-26 10:32:34 +0000

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,12 +6,15 @@ import sitemap from "@astrojs/sitemap"
 
 // https://astro.build/config
 export default defineConfig({
+  build: {
+    inlineStylesheets: 'never'
+  },
   image: {
     service: squooshImageService(),
   },
   integrations: [tailwind({
     // 示例：在每个页面上禁用注入基本的`base.css`导入。
     // 如果你需要定义或导入你自己的 "base.css"，这很有用。
-    applyBaseStyles: false
+    
   }), mdx(), sitemap()]
 });
