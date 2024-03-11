9e51c6043582cbd1552ad2d994197e0111e6dc56
lkzwc <lkzwc@outlook.com>
2024-03-11 22:54:56 +0800

    update blog

--- src/content/config.ts
+++ src/content/config.ts
@@ -42,9 +42,32 @@ const blogCollection = defineCollection({
       tags: z.array(z.string())
     })
  });
+
+
+ const wifi = defineCollection({
+  type: 'content',
+  schema: ({ image })=>z.object({
+    title: z.string(),
+    pubDate: z.string(),
+    description: z.string(),
+    cover:  image().refine((img) => img.width >= 580, {
+      message: "封面图片必须至少 1080 像素宽！",
+    }),
+    coverAlt: z.string(),
+    author: z.string(),
+    image: z.object({
+      url: image().refine((img) => img.width >= 580, {
+        message: "封面图片必须至少 1080 像素宽！",
+        }).or(z.string()).optional(),
+      alt: z.string()
+    }),
+    tags: z.array(z.string())
+  })
+});
 // 3. 导出一个 `collections` 对象来注册集合
 //    这个键应该与 `src/content` 中的集合目录名匹配
 export const collections = {
   'blog': blogCollection,
-  'hotcard': hotCard
+  'hotcard': hotCard,
+  'wifi':wifi
 };
