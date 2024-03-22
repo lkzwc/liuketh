ca357498848272849d086ca78dd34d14cb1b26ab
流客 <lkzwc@outlook.com>
2024-03-22 09:52:46 +0800

    Update config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -70,7 +70,7 @@ const blogCollection = defineCollection({
 // 3. 导出一个 `collections` 对象来注册集合
 //    这个键应该与 `src/content` 中的集合目录名匹配
 export const collections = {
-  'blog': blogCollection,
+  'news': blogCollection,
   'hotcard': hotCard,
   'wifi':wifi
 };
