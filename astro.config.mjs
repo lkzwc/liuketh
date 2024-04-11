953c50e086ae697d2155113fcf8cc11cffc245c1
lkzwc <lkzwc@outlook.com>
2024-04-11 21:41:07 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://liuketh.cn',
+  site: 'https://www.liuketh.cn',
   build: {
     inlineStylesheets: 'never'
   },
