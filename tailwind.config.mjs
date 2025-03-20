726f869a2051150bf603cd4968d2504217cdb6bd
lkzwc <lkzwc@outlook.com>
2025-03-20 22:08:52 +0800

    update

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,7 +6,28 @@ export default {
 		"./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
 	],
 	theme: {
-		extend: {},
+		extend: {
+			animation: {
+				marquee: 'marquee 5s linear infinite',
+				glow: 'glow 5s ease-in-out infinite',
+			  },
+			  keyframes: {
+				marquee: {
+				  '0%': { transform: 'translateX(0%)' },
+				  '100%': { transform: 'translateX(-80%)' },
+				},
+				glow: {
+				  '0%, 100%': { 
+					'text-shadow': '0 0 10px #fff, 0 0 20px #ff0000',
+					'filter': 'brightness(1.2)'
+				  },
+				  '50%': { 
+					'text-shadow': '0 0 20px #ff0000, 0 0 30px #ff0000',
+					'filter': 'brightness(1.5)'
+				  }
+				}
+			  }
+		},
 	},
 	darkMode: "class",
 	plugins: [
